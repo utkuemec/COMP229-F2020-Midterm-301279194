daf2257e29b471b82ae4bfe4bd3e3d033cadff03
utkuemecan <37334604+utkuemecan@users.noreply.github.com>
2022-10-22 15:07:32 -0400

    first commit

---
+++ config/db.js
@@ -0,0 +1,7 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://Utkuemec:Utkuemec@cluster0.jfdl5bg.mongodb.net/mean-midterm?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+    
+}
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index');
+
+/* GET home page. */
+router.get('/', indexController.displayHomePage);
+
+/* GET home page. */
+router.get('/home', indexController.displayHomePage);
+
+/* GET About Us page. */
+router.get('/list', indexController.displayListPage);
+
+/* GET Products page. */
+router.get('/details', indexController.displayDetailsPage);
+
+/* GET Route for displaying the Login page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post('/login', indexController.processLoginPage);
+
+/* GET Route for displaying the Register page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Route for processing the Register page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
+
+module.exports = router;
